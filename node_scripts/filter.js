61bf398c78d48fa3bb93848be7628fb8a33c0fdf
joshua <joshua.onwezen@gmail.com>
2020-02-27 16:47:32 +0100

    Updated to represent xposts with more than 2 shared following subreddits

--- node_scripts/filter.js
+++ node_scripts/filter.js
@@ -2,17 +2,20 @@ var fs = require('fs')
 var csv = require('csv-parser')
 
 var nodeArr = []
+var nodesUsed = []
 var linkArr = []
 var linkUsed = []
-var nodesUsed = []
-var nodesTargetted = []
-var nodeValues = []
+var linksTargetted = []
 var subreddits = [
   'programmerhumor',
   'leagueoflegends',
-  'wow',
-  'MovieDetails',
-  'smashbros'
+  'videos',
+  'askreddit',
+  'smashbros',
+  'askscience',
+  'nintendo',
+  'explainlikeimfive',
+  'wow'
 ]
 
 fs.readFile('../src/resources/datalinks.json', function read (err, data) {
@@ -27,54 +30,119 @@ fs.readFile('../src/resources/datalinks.json', function read (err, data) {
     }
     nodeArr.push(node)
     nodesUsed.push(subreddits[i])
-
   }
 
   for (var i = 0; i < data.links.length; i++) {
     for (var y = 0; y < subreddits.length; y++) {
-      if (subreddits.includes(data.links[i].source)) {
-        var node = {
-          id: data.links[i].target,
-          name: data.links[i].target
-        }
+      if (subreddits[y] == data.links[i].source) {
+        //SUBREDDIT USED AS SOURCE
         if (!nodesUsed.includes(data.links[i].target)) {
-          nodeArr.push(node)
+          //NOT INCLUDED YET? -> ADD TO NODEUSED
+          var node = {
+            id: data.links[i].target,
+            name: data.links[i].target
+          }
           nodesUsed.push(data.links[i].target)
-        }
-        var link = {
-          source: data.links[i].source,
-          target: data.links[i].target
-        }
-        if(!linkUsed.includes(data.links[i].target)){
-          linkArr.push(link)
-        }
-        linkUsed.push(data.links[i].target);
-      } else if (subreddits.includes(data.links[i].target)) {
-        var node = {
-          id: data.links[i].source,
-          name: data.links[i].source
-        }
-        if (!nodesUsed.includes(data.links[i].source)) {
           nodeArr.push(node)
-          nodesUsed.push(data.links[i].source)
         }
+
         var link = {
           source: data.links[i].source,
           target: data.links[i].target
         }
-        if(!linkUsed.includes(data.links[i].source)){
-          linkArr.push(link)
+
+        linkArr.push(link)
+
+        if (linksTargetted[data.links[i].target] && Array.isArray(linksTargetted[data.links[i].target])) {
+          if(!linksTargetted[data.links[i].target].includes(subreddits[y])){
+            linksTargetted[data.links[i].target].push(subreddits[y])
+          }
+        } else {
+          linksTargetted[data.links[i].target] = [subreddits[y]]
         }
-        linkUsed.push(data.links[i].source);
+      } else if (subreddits[y] == data.links[i].target) {
+        //SUBREDDIT USED AS TARGET
+        if (!nodesUsed.includes(data.links[i].source)) {
+          //NOT INCLUDED YET? -> ADD TO NODEUSED
+          var node = {
+            id: data.links[i].source,
+            name: data.links[i].source
+          }
+          nodesUsed.push(data.links[i].source)
+          nodeArr.push(node)
+        }
+
+        var link = {
+          source: data.links[i].source,
+          target: data.links[i].target
+        }
+
+        linkArr.push(link)
+
+        if (linksTargetted[data.links[i].source] && Array.isArray(linksTargetted[data.links[i].source])) {
+          if(!linksTargetted[data.links[i].source].includes(subreddits[y])){
+            linksTargetted[data.links[i].source].push(subreddits[y])
+          }
+        } else {
+          linksTargetted[data.links[i].source] = [subreddits[y]]
+        }
       }
     }
   }
 
-  console.log(nodesUsed)
+  console.log(linksTargetted);
+  for (var i = 0; i < linkArr.length; i++) {
+
+    if (
+      !subreddits.includes(linkArr[i].target) &&
+      linksTargetted[linkArr[i].target] != undefined &&
+      linksTargetted[linkArr[i].target].length > 1
+    ) {
+      linkUsed.push({
+        source: linkArr[i].source,
+        target: linkArr[i].target
+        // weight: linksTargetted[linkArr[i].target]
+      })
+    } else if (
+      !subreddits.includes(linkArr[i].source) &&
+      linksTargetted[linkArr[i].source] != undefined &&
+      linksTargetted[linkArr[i].source].length > 1
+    ) {
+      linkUsed.push({
+        source: linkArr[i].source,
+        target: linkArr[i].target
+        // weight: linksTargetted[linkArr[i].target]
+      })
+    }
+  }
+
+  var nodes = [];
+  nodesUsed = [];
+  for(var i = 0; i < linkUsed.length; i++){
+    if(!nodesUsed.includes(linkUsed[i].target)){
+      var node = {
+        id: linkUsed[i].target,
+        name: linkUsed[i].target
+      }
+      nodes.push(node);
+      nodesUsed.push(linkUsed[i].target);
+    }
+    if(!nodesUsed.includes(linkUsed[i].source)){
+      var node = {
+        id: linkUsed[i].source,
+        name: linkUsed[i].source
+      }
+      nodes.push(node);
+      nodesUsed.push(linkUsed[i].source);
+
+    }
+  }
+
+
 
   var output = {
-    nodes: nodeArr,
-    links: linkArr
+    nodes: nodes,
+    links: linkUsed
   }
   fs.writeFileSync('datalinks-v4.json', JSON.stringify(output))
 })
--- src/scripts/global.js
+++ src/scripts/global.js
@@ -83,7 +83,7 @@ class App {
 
 		var simulation = d3.forceSimulation()
 			.force("link", d3.forceLink().id(function(d) { return d.id; }))
-			.force("charge", d3.forceManyBody().distanceMax(100).distanceMin(25))
+			.force("charge", d3.forceManyBody())
 			.force("center", d3.forceCenter(width / 2, height / 2));
 
 		var link = svg.append("g")
